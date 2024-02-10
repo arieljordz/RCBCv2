af93a00ec339696530d489f2a74076121f10640b
Ariel Sueno <arieljordz@gmail.com>
2024-02-10 23:54:46 +0800

    Update Appsettings Remove Link

--- RCBC/obj/Debug/net7.0/RCBC.AssemblyInfo.cs
+++ RCBC/obj/Debug/net7.0/RCBC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RCBC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89dba9c79ab88e51d12e433eb18345e842cd396a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dab2e93d906a8c37e0d69e20a1fdcd96e4a5cfbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("RCBC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RCBC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
